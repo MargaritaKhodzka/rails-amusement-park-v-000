9120f7c4ff1c38c932da0cd82f864d7f1f7a4cd3
Margarita Khodzka <mkhodzka@gmail.com>
2017-09-07 02:20:21 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   resources :users, except: [:index, :destroy]
   resources :attractions, except: :destroy
+  post '/rides', to: 'rides#create'
   get '/signin', to: 'sessions#new'
   post '/sessions', to: 'sessions#create'
   delete '/signout', to: 'sessions#destroy'
